28eb022c374ac8c6b6d078b650e39215ba07f59c
Mladen <mladenstosic96@gmail.com>
2020-03-13 17:02:38 +0100

    readprog done

--- state.py
+++ state.py
@@ -1,6 +1,6 @@
 class State():
 
-  def __init__(self, name, blank, zero, one):
+  def __init__(self, name, blank = 0, zero = 0, one = 0):
       self.name = name
       self.blank = blank
       self.zero = zero
--- tm.py
+++ tm.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
 import os
 import re
-import state
+from state import State
 
+states = []
 file = open("test.txt", "r")
 
+for i in range(1000):
+    states.append(0)
+
+#read program
 for line in file:
     #print(line)
+    existingstate = False
     newln = re.split('[()=,\n]', line)
     print(newln)
-    #current state
-    print(newln[1])
-    #symbol1
-    print(newln[2])
-    #next state
-    print(newln[5])
-    #new symbol
-    print(newln[6])
-    #move
-    print(newln[7])
-    # for chars in newln:
-    #     chars.rsplit("(")
-    #     print(chars)
+    print(states[int(newln[1][1:])])
 
-input("...")
+    if states[int(newln[1][1:], 10)] != 0:
+        existingstate = True
+    if not existingstate:
+        states[int(newln[1][1:])] = State(newln[1])
+    if newln[2] == 'b':
+        states[int(newln[1][1:])].blank = [newln[5], newln[6], newln[7]]
+    elif newln[2] == '0':
+        states[int(newln[1][1:])].zero = [newln[5], newln[6], newln[7]]
+    elif newln[2] == '1':
+        states[int(newln[1][1:])].one = [newln[5], newln[6], newln[7]]
+    else:
+        print("error")
+        input("...error...")
+
+    #test print    
+    if states[100]!= 0:
+        print(states[100].name)
+        print(states[100].blank)
+        print(states[100].zero)
+        print(states[100].one)
 file.close()
+input("...")
